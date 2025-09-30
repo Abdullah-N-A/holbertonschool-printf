9a53cf18bfdf0bad1643bef5719073c540799017
Abdullah-N-A <abdullahttlrx@gmail.com>
2025-09-30 08:26:04 +0000

    Fix _printf.c: correct output handling and apply Betty style

--- printf.c
+++ printf.c
@@ -3,63 +3,69 @@
 #include <stdarg.h>
 
 /**
- * _printf - simplified version of printf
- * @format: the string that may contain format specifiers
- * Return: number of printed characters
+ * _printf - produces output according to a format
+ * @format: character string with format specifiers
+ *
+ * Return: number of characters printed (excluding '\0'),
+ *         or -1 if error
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0, count = 0;
-    char *s;
+	va_list args;
+	int i = 0, count = 0;
+	char *s;
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format[i]) /* loop until end of string */
-    {
-        if (format[i] == '%') /* check for specifier */
-        {
-            i++;
-            if (format[i] == 'c') /* %c -> print char */
-            {
-                char c = (char)va_arg(args, int);
-                write(1, &c, 1);
-                count++;
-            }
-            else if (format[i] == 's') /* %s -> print string */
-            {
-                s = va_arg(args, char *);
-                if (s == NULL)
-                    s = "(null)";
-                while (*s)
-                {
-                    write(1, s++, 1);
-                    count++;
-                }
-            }
-            else if (format[i] == '%') /* %% -> print % */
-            {
-                write(1, "%", 1);
-                count++;
-            }
-            else /* unknown specifier -> print as is */
-            {
-                write(1, "%", 1);
-                write(1, &format[i], 1);
-                count += 2;
-            }
-        }
-        else /* normal char */
-        {
-            write(1, &format[i], 1);
-            count++;
-        }
-        i++;
-    }
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (!format[i]) /* handle case of % at end */
+				return (-1);
 
-    va_end(args);
-    return (count);
+			if (format[i] == 'c')
+			{
+				char c = va_arg(args, int);
+
+				write(1, &c, 1);
+				count++;
+			}
+			else if (format[i] == 's')
+			{
+				s = va_arg(args, char *);
+				if (!s)
+					s = "(null)";
+				while (*s)
+				{
+					write(1, s, 1);
+					s++;
+					count++;
+				}
+			}
+			else if (format[i] == '%')
+			{
+				write(1, "%", 1);
+				count++;
+			}
+			else
+			{
+				write(1, "%", 1);
+				write(1, &format[i], 1);
+				count += 2;
+			}
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
 }
