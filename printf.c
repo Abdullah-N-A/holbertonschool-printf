06772d05dd380294dcb1ee5f43447d7565dc8729
Abdullah-N-A <abdullahttlrx@gmail.com>
2025-09-30 08:45:24 +0000

    rm main.c

--- printf.c
+++ printf.c
@@ -1,10 +1,25 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * print_char - prints a single character
+ * @args: va_list containing the character
+ * Return: 1 (number of chars printed)
+ */
 int print_char(va_list args)
 {
 	char c = va_arg(args, int);
+
 	write(1, &c, 1);
 	return (1);
 }
 
+/**
+ * print_string - prints a string
+ * @args: va_list containing the string
+ * Return: number of chars printed
+ */
 int print_string(va_list args)
 {
 	char *s = va_arg(args, char *);
@@ -20,6 +35,11 @@ int print_string(va_list args)
 	return (count);
 }
 
+/**
+ * _printf - custom printf function supporting %c, %s, %%
+ * @format: format string containing specifiers
+ * Return: number of characters printed, -1 if error
+ */
 int _printf(const char *format, ...)
 {
 	va_list args;
@@ -29,13 +49,18 @@ int _printf(const char *format, ...)
 		return (-1);
 
 	va_start(args, format);
+
 	while (format[i])
 	{
 		if (format[i] == '%')
 		{
 			i++;
 			if (!format[i])
+			{
+				va_end(args);
 				return (-1);
+			}
+
 			if (format[i] == 'c')
 				count += print_char(args);
 			else if (format[i] == 's')
@@ -59,6 +84,8 @@ int _printf(const char *format, ...)
 		}
 		i++;
 	}
+
 	va_end(args);
 	return (count);
 }
+
