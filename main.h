3de799cb65c2c39d497d76afb60701bd143953ac
Abdullah-N-A <abdullahttlrx@gmail.com>
2025-09-30 08:35:54 +0000

    Add main.c, main.h and update printf.c with _printf implementation

--- main.h
+++ main.h
@@ -4,13 +4,19 @@
 /*
  * File: main.h
  * Auth: Your Name
- * Desc: Header file containing all function prototypes for the
- *       custom printf project (holbertonschool-printf).
+ * Desc: Header file for custom _printf function
+ *       Supports %c, %s, and %% specifiers.
  */
+
 #include <stdarg.h>
 #include <unistd.h>
 
 /* Prototype for _printf */
 int _printf(const char *format, ...);
 
+/* Helper functions prototypes */
+int print_char(va_list args);
+int print_string(va_list args);
+
 #endif /* MAIN_H */
+
--- printf.c
+++ printf.c
@@ -1,52 +1,45 @@
-#include "main.h"
-#include <unistd.h>
-#include <stdarg.h>
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+	write(1, &c, 1);
+	return (1);
+}
 
-/**
- * _printf - produces output according to a format
- * @format: character string with format specifiers
- *
- * Return: number of characters printed (excluding '\0'),
- *         or -1 if error
- */
+int print_string(va_list args)
+{
+	char *s = va_arg(args, char *);
+	int count = 0;
+
+	if (!s)
+		s = "(null)";
+	while (*s)
+	{
+		write(1, s++, 1);
+		count++;
+	}
+	return (count);
+}
+
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int i = 0, count = 0;
-	char *s;
 
-	if (format == NULL)
+	if (!format)
 		return (-1);
 
 	va_start(args, format);
-
 	while (format[i])
 	{
 		if (format[i] == '%')
 		{
 			i++;
-			if (!format[i]) /* handle case of % at end */
+			if (!format[i])
 				return (-1);
-
 			if (format[i] == 'c')
-			{
-				char c = va_arg(args, int);
-
-				write(1, &c, 1);
-				count++;
-			}
+				count += print_char(args);
 			else if (format[i] == 's')
-			{
-				s = va_arg(args, char *);
-				if (!s)
-					s = "(null)";
-				while (*s)
-				{
-					write(1, s, 1);
-					s++;
-					count++;
-				}
-			}
+				count += print_string(args);
 			else if (format[i] == '%')
 			{
 				write(1, "%", 1);
