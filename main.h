e13c9d5db7f5e67bb2f4576f22eec4da1d952879
Fahd36631 <Fahd36631@gmail.com>
2025-10-02 12:06:35 +0000

    Add  prototype for print_unsigned function to main.h

--- main.h
+++ main.h
@@ -12,4 +12,7 @@ int print_string(char *s);
 
 int print_int(int n);
 int print_binary(unsigned int n);
+int print_unsigned(unsigned int n);
+int print_octal(unsigned int n);
+int print_hex(unsigned int n, int uppercase);
 #endif
